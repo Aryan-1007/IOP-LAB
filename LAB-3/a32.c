73208b74e5a299085122ee314361fcc4f014a0d8
Aryan-1007 <u25ai059@aid.svnit.ac.in>
2025-09-17 14:44:38 +0530

    ASSIGNMENT

---
+++ LAB-3/a32.c
@@ -0,0 +1,28 @@
+//LAB-3 Q-32
+/*PROGRAM TO FINDOUT MAX AND SECOND MAX FROM ‘N’ NOS*/
+#include<stdio.h>
+
+int main(int argc, char const *argv[])
+{   int n,max,second_max;         //DECLARATION OF VARIABLE 
+    printf("Enter the number of digits:\n");
+    scanf("%d",&n);
+    int arr[n];
+    printf("Enter the digits\n");
+    for(int i=0;i<n;i++)
+    {
+    scanf("%d",&arr[i]);
+    }
+    max=arr[0];
+    second_max=arr[0];
+    for(int i=0;i<n-1;i++)  
+    {
+        if(arr[i]>max)       //CONDITION TO CHECK FOR MAXIMUM AND SECOND MAXIMUM
+        {
+            second_max=max;
+            max=arr[i];
+        }
+    }
+    printf("The maximum of the following is %d\n",max);
+    printf("The second maximum number of the following is %d",second_max);
+    return 0;
+}
---
+++ LAB-3/a33.c
@@ -0,0 +1,33 @@
+//LAB-3 Q-33
+/*PROGRAM TO FINDOUT MAX AND MIN FROM ‘N’ NOS.*/
+
+#include<stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int n,max,mini;            //DECLARATION OF VARIABLES
+    printf("Enter the number of digits:\n");
+    scanf("%d",&n);
+    int arr[n];
+    printf("Enter the digits\n");
+    for(int i=0;i<n;i++)
+    {
+    scanf("%d",&arr[i]);
+    }
+    max=arr[0];
+    mini=arr[0];
+    for(int i=0;i<n-1;i++)
+    {
+        if(arr[i]>max)      //CONDITION TO CHECK FOR MAXIMUM
+        {
+            max=arr[i];
+        }
+        if(arr[i]<mini)     //CONDITION TO CHECK FOR MINIMUM
+        {
+            mini=arr[i];
+        }
+    }
+    printf("The maximum of the following is %d\n",max);
+    printf("The minimum of the following is %d",mini);
+    return 0;
+}//END OF THE PROGRAM
