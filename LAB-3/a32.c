6c9a74750a63fc0aa9f88a8668b86bcae3882788
Aryan-1007 <u25ai059@aid.svnit.ac.in>
2025-09-17 15:12:26 +0530

    ASSIGNMENT

--- LAB-3/a32.c
+++ LAB-3/a32.c
@@ -1,29 +1,30 @@
-//LAB-3 Q-32
+// LAB-3 Q-32
 /*PROGRAM TO FINDOUT MAX AND SECOND MAX FROM ‘N’ NOS*/
-#include<stdio.h>
+#include <stdio.h>
 
 int main(int argc, char const *argv[])
-{   int n,max,second_max;         //DECLARATION OF VARIABLE 
+{
+    int n, max, second_max; // DECLARATION OF VARIABLE
     printf("Enter the number of digits:\n");
-    scanf("%d",&n);
+    scanf("%d", &n);
     int arr[n];
     printf("Enter the digits\n");
-    for(int i=0;i<n;i++)
+    for (int i = 0; i < n; i++)
     {
-    scanf("%d",&arr[i]);
+        scanf("%d", &arr[i]);
     }
-    max=arr[0];
-    second_max=arr[0];
-    for(int i=0;i<n-1;i++)  
+    max = arr[0];
+    second_max = arr[0];
+    for (int i = 0; i < n - 1; i++)
     {
-        if(arr[i]>max)       //CONDITION TO CHECK FOR MAXIMUM AND SECOND MAXIMUM
+        if (arr[i] > max) // CONDITION TO CHECK FOR MAXIMUM AND SECOND MAXIMUM
         {
-            second_max=max;
-            max=arr[i];
+            second_max = max;
+            max = arr[i];
         }
     }
-    printf("The maximum of the following is %d\n",max);
-    printf("The second maximum number of the following is %d",second_max);
+    printf("The maximum of the following is %d\n", max);
+    printf("The second maximum number of the following is %d", second_max);
     return 0;
 }
-//END OF THE PROGRAM
+// END OF THE PROGRAM
