54a044530229e72f58b32e0d2df2daab7bef1162
Aryan Patel <patelaryank1007@gmail.com>
2025-09-30 22:22:03 +0530

    ASSIGNMENT

--- CASE STUDY/Biggerisgreater.c
+++ CASE STUDY/Biggerisgreater.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-int stringintoascii(char arr[])
+long long stringintoascii(char arr[])
 {
     int asc = 0;
     for (int i = 0; arr[i] != '\0'; i++)
@@ -21,9 +21,8 @@ int main()
         char word[101], ans[101] = "";
         scanf("%s", word);
 
-        int min = stringintoascii(word);
-        int max = 999999999;
-
+        long long min = stringintoascii(word);
+        long long max = 999999999999999999;
         for (int j = 0; word[j] != '\0'; j++)
         {
             for (int k = j + 1; word[k] != '\0'; k++)
@@ -32,10 +31,11 @@ int main()
                 word[j] = word[k];
                 word[k] = temp;
 
-                int temp_val = stringintoascii(word);
-                if (temp_val >= min && temp_val <= max)
+                long long temp_val = stringintoascii(word);
+                if (temp_val > min && temp_val <= max)
                 {
                     strcpy(ans, word);
+                    max = stringintoascii(word);
                 }
                 temp = word[j];
                 word[j] = word[k];
@@ -46,7 +46,7 @@ int main()
         if (strlen(ans) > 0)
             printf("%s\n", ans);
         else
-            printf("%s\n", word);
+            printf("no answer\n");
     }
 
     return 0;
